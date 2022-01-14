28ea53df91d09cd4471e9934e7ca967ef66dedd8
TheGooodDev <72625343+TheGooodDev@users.noreply.github.com>
2022-01-14 23:21:53 +0100

    feat(folder)
    Tout les dossiers pour commencer
    
    Asset/Model -> pour model 3D
    
    Asset/Texture -> Pour les texture des block / item
    
    Data/ -> json pour les drop / recettes

--- src/main/java/com/IMC/ynov/Start.java
+++ src/main/java/com/IMC/ynov/Start.java
@@ -1,9 +1,11 @@
 package com.IMC.ynov;
 
+import com.IMC.ynov.item.Moditems;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
@@ -27,11 +29,16 @@ public class Start
 
     public Start() {
         // Register the setup method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+
+        Moditems.register(eventBus);
+
+        eventBus.addListener(this::setup);
         // Register the enqueueIMC method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
+        eventBus.addListener(this::enqueueIMC);
         // Register the processIMC method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
+        eventBus.addListener(this::processIMC);
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
