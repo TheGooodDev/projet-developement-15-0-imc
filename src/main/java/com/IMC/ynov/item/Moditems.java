b3a823245e5d186a3ed4ad0663f50fdf0eb095b9
TheGooodDev <72625343+TheGooodDev@users.noreply.github.com>
2022-01-15 00:57:02 +0100

    fix(Build, File, Directory,) Feat(item)
    Changé le mapping : Official -> Parchment
    
    Item :
    -json lang
    -Texture
    
    C'est un exemple

--- src/main/java/com/IMC/ynov/item/Moditems.java
+++ src/main/java/com/IMC/ynov/item/Moditems.java
@@ -1,6 +1,7 @@
 package com.IMC.ynov.item;
 
-import com.IMC.ynov.Start;
+import com.IMC.ynov.Mod;
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -9,10 +10,13 @@ import net.minecraftforge.registries.RegistryObject;
 
 public class Moditems {
 
-    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Start.MOD_ID);
-    public static final Item.Properties ITEM_PROPERTIES = new Item.Properties().tab(ModSetup.ITEM_GROUP)
-    public static final RegistryObject<Item> Hugo = ITEMS.register("hugo",
-            () -> new Item())
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Mod.MOD_ID);
+
+    public static final RegistryObject<Item> hugo = ITEMS.register("hugo",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static final RegistryObject<Item> thomas = ITEMS.register("thomas",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
 
     public static void register(IEventBus eventBus){
         ITEMS.register(eventBus);
