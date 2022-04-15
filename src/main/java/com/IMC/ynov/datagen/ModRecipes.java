ee68d59e4fc7601aaa2947d646a74bfc4c6a8d4a
JLWear <77749373+JLWear@users.noreply.github.com>
2022-04-15 12:43:23 +0200

    Recipe

--- src/main/java/com/IMC/ynov/datagen/ModRecipes.java
+++ src/main/java/com/IMC/ynov/datagen/ModRecipes.java
@@ -1,8 +1,13 @@
 package com.IMC.ynov.datagen;
 
+import com.IMC.ynov.setup.Registration;
+import net.minecraft.advancements.critereon.InventoryChangeTrigger;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.recipes.FinishedRecipe;
 import net.minecraft.data.recipes.RecipeProvider;
+import net.minecraft.data.recipes.ShapedRecipeBuilder;
+import net.minecraft.world.item.Items;
+import net.minecraftforge.common.Tags;
 
 import java.util.function.Consumer;
 
@@ -14,5 +19,140 @@ public class ModRecipes extends RecipeProvider {
 
     @Override
     protected void buildCraftingRecipes(Consumer<FinishedRecipe> consumer) {
+        ShapedRecipeBuilder.shaped(Registration.PEAR_BUTTON.get())
+                .pattern("#")
+                .define('#', Registration.PEAR_PLANKS.get())
+                .group("ynov")
+                .save(consumer);
+        ShapedRecipeBuilder.shaped(Registration.PEAR_SLAB.get())
+                .pattern("###")
+                .define('#', Registration.PEAR_PLANKS.get())
+                .group("ynov")
+                .save(consumer);
+        ShapedRecipeBuilder.shaped(Registration.PEAR_STAIRS.get())
+                .pattern("#")
+                .pattern("##")
+                .pattern("###")
+                .define('#', Registration.PEAR_PLANKS.get())
+                .group("ynov")
+                .save(consumer);
+        ShapedRecipeBuilder.shaped(Registration.PEAR_PRESSURE_PLATE.get())
+                .pattern("##")
+                .define('#', Registration.PEAR_PLANKS.get())
+                .group("ynov")
+                .save(consumer);
+        ShapedRecipeBuilder.shaped(Registration.PEAR_TRAPDOOR.get())
+                .pattern("##")
+                .pattern("##")
+                .define('#', Registration.PEAR_PLANKS.get())
+                .group("ynov")
+                .save(consumer);
+        ShapedRecipeBuilder.shaped(Registration.PEAR_DOOR.get())
+                .pattern("##")
+                .pattern("##")
+                .pattern("##")
+                .define('#', Registration.PEAR_PLANKS.get())
+                .group("ynov")
+                .save(consumer);
+        ShapedRecipeBuilder.shaped(Registration.PEAR_PLANKS.get())
+                .pattern("#")
+                .define('#', Registration.PEAR_LOG.get())
+                .group("ynov")
+                .save(consumer);
+
+
+        ShapedRecipeBuilder.shaped(Registration.BANANA_BUTTON.get())
+                .pattern("#")
+                .define('#', Registration.BANANA_PLANKS.get())
+                .group("ynov")
+                .save(consumer);
+        ShapedRecipeBuilder.shaped(Registration.BANANA_SLAB.get())
+                .pattern("###")
+                .define('#', Registration.BANANA_PLANKS.get())
+                .group("ynov")
+                .save(consumer);
+        ShapedRecipeBuilder.shaped(Registration.BANANA_STAIRS.get())
+                .pattern("#")
+                .pattern("##")
+                .pattern("###")
+                .define('#', Registration.BANANA_PLANKS.get())
+                .group("ynov")
+                .save(consumer);
+        ShapedRecipeBuilder.shaped(Registration.BANANA_PRESSURE_PLATE.get())
+                .pattern("##")
+                .define('#', Registration.BANANA_PLANKS.get())
+                .group("ynov")
+                .save(consumer);
+        ShapedRecipeBuilder.shaped(Registration.BANANA_TRAPDOOR.get())
+                .pattern("##")
+                .pattern("##")
+                .define('#', Registration.BANANA_PLANKS.get())
+                .group("ynov")
+                .save(consumer);
+        ShapedRecipeBuilder.shaped(Registration.BANANA_DOOR.get())
+                .pattern("##")
+                .pattern("##")
+                .pattern("##")
+                .define('#', Registration.BANANA_PLANKS.get())
+                .group("ynov")
+                .save(consumer);
+        ShapedRecipeBuilder.shaped(Registration.BANANA_PLANKS.get())
+                .pattern("#")
+                .define('#', Registration.BANANA_LOG.get())
+                .group("ynov")
+                .save(consumer);
+
+
+        ShapedRecipeBuilder.shaped(Registration.ORANGE_BUTTON.get())
+                .pattern("#")
+                .define('#', Registration.ORANGE_PLANKS.get())
+                .group("ynov")
+                .save(consumer);
+        ShapedRecipeBuilder.shaped(Registration.ORANGE_SLAB.get())
+                .pattern("###")
+                .define('#', Registration.ORANGE_PLANKS.get())
+                .group("ynov")
+                .save(consumer);
+        ShapedRecipeBuilder.shaped(Registration.ORANGE_STAIRS.get())
+                .pattern("#")
+                .pattern("##")
+                .pattern("###")
+                .define('#', Registration.ORANGE_PLANKS.get())
+                .group("ynov")
+                .save(consumer);
+        ShapedRecipeBuilder.shaped(Registration.ORANGE_PRESSURE_PLATE.get())
+                .pattern("##")
+                .define('#', Registration.ORANGE_PLANKS.get())
+                .group("ynov")
+                .save(consumer);
+        ShapedRecipeBuilder.shaped(Registration.ORANGE_TRAPDOOR.get())
+                .pattern("##")
+                .pattern("##")
+                .define('#', Registration.ORANGE_PLANKS.get())
+                .group("ynov")
+                .save(consumer);
+        ShapedRecipeBuilder.shaped(Registration.ORANGE_DOOR.get())
+                .pattern("##")
+                .pattern("##")
+                .pattern("##")
+                .define('#', Registration.ORANGE_PLANKS.get())
+                .group("ynov")
+                .save(consumer);
+        ShapedRecipeBuilder.shaped(Registration.ORANGE_PLANKS.get())
+                .pattern("#")
+                .define('#', Registration.ORANGE_LOG.get())
+                .group("ynov")
+                .save(consumer);
+
+        ShapedRecipeBuilder.shaped(Registration.FRUIT_SALAD.get())
+                .pattern(" #")
+                .pattern("abc")
+                .pattern(" d")
+                .define('#', Items.APPLE)
+                .define('a', Registration.BANANA.get())
+                .define('b', Registration.PEAR.get())
+                .define('c', Registration.ORANGE.get())
+                .define('d', Items.BOWL)
+                .save(consumer);
     }
 }
